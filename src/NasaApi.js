c80a9f2b8856b9741abadd80a8bba742d05e03bf
AlisaFrolova <alisafrolova.dm@gmail.com>
2026-09-15 20:36:51 +0300

    APOD works!

--- src/NasaApi.js
+++ src/NasaApi.js
@@ -1,1 +1,15 @@
 //tOrrEVAmkqSnlbf5QcUBPCrfWOuqPplOzbi2JveA
+ const APIKey = "tOrrEVAmkqSnlbf5QcUBPCrfWOuqPplOzbi2JveA"
+export default async function getAPOD(date){ //date in YYYY-MM-DD only. or just empty string to get the freshest image
+    try{
+        const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${APIKey}&date=${date}`)
+        if(!response.ok){
+            throw new Error(`Error: ${response.status}`)
+        }
+        const data = await response.json()
+        console.log(data)
+        return data
+    } catch(error){
+        console.log(error)
+    }
+}
--- src/views/SpaceView.js
+++ src/views/SpaceView.js
@@ -1,13 +1,24 @@
 import createEl from "./tools.js"
+import getAPOD from "../NasaApi.js"
 
 export default function spaceView(){
     const app = document.querySelector('#app')
 
     //section
-    const section = document.createElement("section")
+    const section = createEl("section", "", "spaceSection")
     app.append(section)
 
     section.append(createEl("h1", "Space", "header"))
+
+    const APODSection = createEl("section", "", "APODSection")
+
+    getAPOD("").then(picture => {
+        APODSection.style.backgroundImage = `url(${picture.url})`
+    })
+
+    section.append(APODSection)
+
+    APODSection.append(createEl("p", "Astronomy Picture of the Day", "textWhite"))
 
     return section
 }
