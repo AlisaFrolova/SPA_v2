b0581c77f4aee30655c81997987c2715a28e56b8
AlisaFrolova <alisafrolova.dm@gmail.com>
2026-09-20 17:16:41 +0300

    sort section, NF

--- src/views/spaceView/Modules/asteroidModule.js
+++ src/views/spaceView/Modules/asteroidModule.js
@@ -2,6 +2,7 @@ import { getAsteroids } from "../../../NasaApi.js"
 import createEl from "../../tools.js"
 
 export default function createAsteroidSection(){
+    const asteroidSection = createEl("section", "", "asteroidSection")
     const asteroidContainer = createEl("div", "", "asteroidContainer")
     asteroidContainer.style.backgroundImage = `url(https://images-assets.nasa.gov/image/iss074e0472536/iss074e0472536~orig.jpg)`
     getAsteroids().then(asteroids => { //ADD SOME VALIDATION BEFORE !
@@ -9,44 +10,100 @@ export default function createAsteroidSection(){
         spawnCards(asteroidContainer, asteroids.near_earth_objects["2026-09-10"], "https://images-assets.nasa.gov/image/PIA15506/PIA15506~orig.jpg")
         spawnCards(asteroidContainer, asteroids.near_earth_objects["2026-09-11"], "https://images-assets.nasa.gov/image/PIA02471/PIA02471~orig.jpg")
     })
-    return asteroidContainer
+    asteroidSection.append(asteroidContainer)
+
+    asteroidSection.prepend(createSortMenu())
+
+    return asteroidSection
 }
 
 function createAsteroidCard(asteroidName, approachDate, diameter, lunarDistance, hazardous, imageSource){
-        const card = createEl("div", "", "asteroidCard")
+    const card = createEl("div", "", "asteroidCard")
 
-        const img = createEl("img", "", "asteroidImage")
-        img.src = imageSource
-        card.append(img)
+    const img = createEl("img", "", "asteroidImage")
+    img.src = imageSource
+    card.append(img)
 
-        card.append(createEl("p", asteroidName, "accent"))
+    card.append(createEl("p", asteroidName, "accent"))
 
-        card.append(createEl("p", `Date: ${approachDate}`, "textWhite"))
+    card.append(createEl("p", `Date: ${approachDate}`, "textWhite"))
 
-        card.append(createEl("p", `Diameter: ${diameter} m`, "textWhite"))
+    card.append(createEl("p", `Diameter: ${diameter} m`, "textWhite"))
 
-        card.append(createEl("p", `Lunar Distance: ${lunarDistance} LD`, "textWhite"))
+    card.append(createEl("p", `Lunar Distance: ${lunarDistance} LD`, "textWhite"))
 
-        const asteroidStatus = document.createElement("p")
-        if(hazardous){
-            asteroidStatus.textContent = "Hazardous Asteroid"
-            asteroidStatus.classList.add("hazardousAsteroid")
-        } else{
-            asteroidStatus.textContent = "Safe Asteroid"
-            asteroidStatus.classList.add("safeAsteroid")
-        }
-        card.append(asteroidStatus)
-        return card
+    const asteroidStatus = document.createElement("p")
+    if(hazardous){
+        asteroidStatus.textContent = "Hazardous Asteroid"
+        asteroidStatus.classList.add("hazardousAsteroid")
+    } else{
+        asteroidStatus.textContent = "Safe Asteroid"
+        asteroidStatus.classList.add("safeAsteroid")
     }
+    card.append(asteroidStatus)
+    return card
+}
 
-    function spawnCards(container, arr, imgLink){
-        for (const el of arr) {
-            container.append(createAsteroidCard(el.name,
-                 el.close_approach_data[0].close_approach_date_full,
-                  el.estimated_diameter.meters.estimated_diameter_min,
+function spawnCards(container, arr, imgLink){
+    for (const el of arr) {
+        container.append(createAsteroidCard(el.name,
+            el.close_approach_data[0].close_approach_date_full,
+                el.estimated_diameter.meters.estimated_diameter_min,
                    el.close_approach_data[0].miss_distance.lunar,
-                    el.is_potentially_hazardous_asteroid, imgLink))
-        }
+                       el.is_potentially_hazardous_asteroid, imgLink))
     }
+}
 
+function createSortMenu(){
+    const asteroidSortMenu = createEl("div", "", "asteroidSortMenu")
+
+    asteroidSortMenu.append(createEl("h2", "Filter Asteroids", "textWhite"))
+
+    const asteroidSortContainer = createEl("div", "", "asteroidSortContainer")
+    asteroidSortContainer.append(createSortBlock("range", "Diameter"))
+    asteroidSortContainer.append(createSortBlock("range", "Lunar Distance"))
+    asteroidSortContainer.append(createSortBlock("checkbox", "Hazardous"))
+    asteroidSortMenu.append(asteroidSortContainer)
     
+    const confirmButton = createEl("button", "Apply", "confirmButton")
+    const hazardousInput = document.querySelector(".asteroidSortMenu")//NW!!! parent is not added to the DOM yet
+    console.log(hazardousInput)
+    confirmButton.addEventListener("click", () => {
+        if(hazardousInput.checked){
+            //NF
+        }
+        //NF
+        //examples of filters:
+//         function sortByBrend(brend, arr, input){
+//     if(!input.checked){
+//         arr = arr.filter(el => {
+//             return el.brend !== brend;
+//         })
+//     }
+//     return arr;
+// }
+
+// function sortByPrice(arr){
+//     arr = arr.filter(el => parseInt(el.prices.slice(0, -1)) >= parseInt(price_input.value))
+//     return arr;
+// }
+    })
+    asteroidSortMenu.append(confirmButton)
+
+    return asteroidSortMenu
+}    
+
+function createSortBlock(inputType, inputName){
+    const sortBlock = createEl("div", "", "sortBlock")
+
+    const tempInput = createEl("input", "", "asteroidInput")
+    tempInput.type = inputType
+    tempInput.id = inputName
+    sortBlock.append(tempInput)
+
+    const tempLabel = createEl("label", inputName, "asteroidLabel")
+    tempLabel.for = inputName
+    sortBlock.append(tempLabel)
+
+    return sortBlock
+}
